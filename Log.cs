05dc0c0c0d6ee71b3453293c2cf8a919a1cf2355
sskim <ss.kim@gmail.com>
2020-04-23 10:10:00 +0900

    최초 : 주차 관리 시스템 주차, 출차, 로그 출력 구현

---
+++ Log.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Log
+    {
+        public static void PrintLog(string directoryname, string filename, string contents)
+        {
+            DirectoryInfo di = new DirectoryInfo(directoryname);
+            if (!di.Exists)
+            {
+                di.Create();
+            }
+            using (StreamWriter writer = new StreamWriter(directoryname + "\\" + filename, true))
+            {
+                writer.WriteLine("[" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss" + "]" + contents));
+            }
+        }
+    }
+}
